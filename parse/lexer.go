700bbc2042b987e4696f058a04681bf10718b358
Christoph Frädrich <chris@treebug.de>
2017-07-10 12:33:36 +0200

    Add parser and lexer for todo

---
+++ parse/lexer.go
@@ -0,0 +1,145 @@
+package parse
+
+import (
+	"bufio"
+	"io"
+	"bytes"
+)
+
+type Token int
+
+const (
+	//Special
+	ILLEGAL_TOKEN = iota
+	EOF
+	WS
+
+	//Literals
+	IDENT  //todos and dates
+
+	//Key Symbols
+	STATUS_OPEN   //[
+	STATUS_CLOSE  //]
+	DATE_OPEN     //{
+	DATE_CLOSE    //}
+
+	//Misc
+	ASTERISK  //*
+	COMMA     //,
+	DOT       //.
+)
+
+func isWhitespace(ch rune) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+func isLetter(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+var eof = rune(0)
+
+//Scanner represents a lexical scanner
+type Scanner struct {
+	r *bufio.Reader
+}
+
+//NewScanner returns a new instance of Scanner
+func NewScanner(r io.Reader) *Scanner {
+	return &Scanner{r: bufio.NewReader(r)}
+}
+
+//read reads the next rune from the buffered reader.
+//Returns the rune(0) if an error occurs(or io.EOF is returned).
+func (s *Scanner) read() rune {
+	r, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+
+	return r
+}
+
+//unread places the previously read rune back to the reader
+func (s *Scanner) unread() {
+	s.r.UnreadRune()
+}
+
+//Scan returns the next token and its value
+func (s *Scanner) Scan() (tok Token, lit string) {
+	ch := s.read()
+
+	// If we see whitespace then consume all contiguous whitespace.
+	// If we see a letter then consume as an ident or reserved word.
+	if isWhitespace(ch) {
+		s.unread()
+		return s.scanWhitespace()
+	} else if isLetter(ch) {
+		s.unread()
+		return s.scanIdent()
+	}
+
+	//Otherwise read individual character
+	switch ch {
+	case '{':
+		return DATE_OPEN, "{"
+	case '}':
+		return DATE_CLOSE, "}"
+	case '[':
+		return STATUS_OPEN, "["
+	case ']':
+		return STATUS_CLOSE, "]"
+	case ',':
+		return COMMA, ","
+	case '.':
+		return DOT, "."
+	case '*':
+		return ASTERISK, "*"
+	case eof:
+		return EOF, string(ch)
+	}
+
+	return ILLEGAL_TOKEN, string(ch)
+}
+
+func (s *Scanner) scanWhitespace() (tok Token, lit string) {
+	//Create buffer and read the current character into it
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	//Read every subsequent whitespace into the buffer.
+	//Non Whitspace Characters and EOF will cause the loop to exit
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if isLetter(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return WS, buf.String()
+}
+
+func (s *Scanner) scanIdent() (tok Token, lit string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	//Read every subsequent ident character into the buffer.
+	//Non ident Characters and EOF will cause the loop to exit
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if isWhitespace(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return IDENT, buf.String()
+}
---
+++ parse/parser.go
@@ -0,0 +1,121 @@
+package parse
+
+import (
+	"io"
+	"time"
+	"fmt"
+	"bytes"
+)
+
+type Task struct {
+	description string
+	dueTime     time.Time
+	complete    bool
+}
+
+type Parser struct {
+	s *Scanner
+	buf struct {
+		tok Token  //last read token
+		lit string //last string literal
+		n   int    //buffer size (max = 1)
+	}
+}
+
+//NewParser returns an instance of a new parser
+func NewParser(r io.Reader) *Parser {
+	return &Parser{s: NewScanner(r)}
+}
+
+func (p *Parser) scan() (tok Token, lit string) {
+	//if we have a token on the buffer, return it
+	if p.buf.n != 0 {
+		p.buf.n = 0
+		return p.buf.tok, p.buf.lit
+	}
+
+	//Otherwise read the next token from the scanner
+	tok, lit = p.s.Scan()
+
+	//Save it to the buffer in case we unscan later.
+	p.buf.tok, p.buf.lit = tok, lit
+
+	return
+}
+
+func (p *Parser) scanIgnoreWhitespace() (tok Token, lit string) {
+	tok, lit = p.scan()
+	if tok == WS {
+		tok, lit = p.scan() //shouldn't we use scanIgnore whitespace again?
+	}
+
+	return
+}
+
+func (p *Parser) parse() (*Task, error) {
+	task := &Task{}
+
+	if tok, lit := p.scanIgnoreWhitespace(); tok != STATUS_OPEN {
+		return nil, fmt.Errorf("found %q, expected [", lit)
+	}
+
+	tok, lit := p.scan()
+	if tok != WS && tok != IDENT {
+		return nil, fmt.Errorf("found %q, expected WS or X", lit)
+	}
+
+	if tok == IDENT {
+		task.complete = true
+	} else {
+		task.complete = false
+	}
+
+	if tok, lit := p.scan(); tok != STATUS_CLOSE {
+		return nil, fmt.Errorf("found %q, expected ]", lit)
+	}
+
+	var buf bytes.Buffer
+
+	for {
+		//Read a field
+		tok, lit := p.scan()
+
+		if tok != IDENT && tok != DOT && tok != COMMA && tok != STATUS_OPEN {
+			return nil, fmt.Errorf("found %q, expected field", lit)
+		}
+
+		if tok == STATUS_OPEN {
+			p.s.unread()
+			break
+		}
+
+		buf.WriteString(lit)
+	}
+
+	task.description = buf.String()
+
+	if tok, lit := p.scanIgnoreWhitespace(); tok != DATE_OPEN {
+		return nil, fmt.Errorf("found %q, expected [", lit)
+	}
+
+	buf.Reset()
+	for {
+		tok, lit := p.scanIgnoreWhitespace()
+		if tok != IDENT && tok != DOT && tok != DATE_CLOSE {
+			return nil, fmt.Errorf("found %q, expected field", lit)
+		}
+
+		if tok == DATE_CLOSE {
+			dueTime, err := time.Parse("01.02.06", buf.String())
+			if err != nil {
+				return nil, err
+			}
+			task.dueTime = dueTime
+			break
+		}
+
+		buf.WriteString(lit)
+	}
+
+	return task, nil
+}
